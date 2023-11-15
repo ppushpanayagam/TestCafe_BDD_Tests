e4483988c783836bd44c4dea2fc6774861439958
Pushparaj Pushpanayagam <ppushpanayagam@gmail.com>
2023-11-15 00:25:59 +0000

    added test data json to have data driven approach

--- src/steps/test-steps.js
+++ src/steps/test-steps.js
@@ -1,84 +1,88 @@
 const { Given, When, Then } = require("cucumber");
 const supportMethods = require("../methods/support.js");
+const testData = require("../testData/data.json");
 
-Given('I am on the homepage', async function () {
+testData.forEach(data => {
     
-    await supportMethods
-        .onHomepage();
-});
+    Given('I am on the homepage', async function () {
+        
+        await supportMethods
+            .onHomepage();
+    });
 
-Given('I navigate to special offer page', async function () {
-    
-    await supportMethods
-        .navigateToSpecialOfferPage();
-    await supportMethods
-        .verifySpecialOfferPage();
-});
+    Given('I navigate to special offer page', async function () {
+        
+        await supportMethods
+            .navigateToSpecialOfferPage();
+        await supportMethods
+            .verifySpecialOfferPage();
+    });
 
-When('I change the language to French', async function () {
-    
-    await supportMethods
-        .clickOnLanguageDropDown();
-    await supportMethods
-        .clickOnGivenLanguageFromTheList("France");
-});
+    When('I change the language to French', async function () {
+        
+        await supportMethods
+            .clickOnLanguageDropDown();
+        await supportMethods
+            .clickOnGivenLanguageFromTheList(data.frenchLanguage);
+    });
 
-When('I change the language to Italia', async function () {
-    
-    await supportMethods
-        .clickOnLanguageDropDown();
-    await supportMethods
-        .clickOnGivenLanguageFromTheList("Italia");
-});
+    When('I change the language to Italia', async function () {
+        
+        await supportMethods
+            .clickOnLanguageDropDown();
+        await supportMethods
+            .clickOnGivenLanguageFromTheList(data.italiaLanguage);
+    });
 
-Then('I should see the language changed to French', async function () {
-    
-    await supportMethods
-        .verifyCurrentCountryUrl('directferries.fr')
-});
+    Then('I should see the language changed to French', async function () {
+        
+        await supportMethods
+            .verifyCurrentCountryUrl('directferries.fr')
+    });
 
-Then('I should see the language changed to Italia', async function () {
-    
-    await supportMethods
-        .verifyCurrentCountryUrl('directferries.it')
-});
+    Then('I should see the language changed to Italia', async function () {
+        
+        await supportMethods
+            .verifyCurrentCountryUrl('directferries.it')
+    });
 
-Given('I navigage to the UK my account page', async function () {
-    
-    await supportMethods
-        .navigateToMyAccountPage('https://account.directferries.com/?culture=en-GB');
-});
+    Given('I navigage to the UK my account page', async function () {
+        
+        await supportMethods
+            .navigateToMyAccountPage(data.myAccountPageUrl);
+    });
 
-Given('I login into my account with valid credentials', async function () {
-    
-    await supportMethods
-        .loginToMyBookingPage('qatesting@directferries.com', 'DFP164826683');
+    Given('I login into my account with valid credentials', async function () {
+        
+        await supportMethods
+            .loginToMyBookingPage(data.userName, data.password);
 
-});
+    });
 
-Given('I should see Outbound and return booking details as expected', async function () {
-    
-    await supportMethods
-        .verifyOutboundBookingDetails(2, 1);
-    await supportMethods
-        .verifyInboundBookingDetails(1, 1);
+    Given('I should see Outbound and return booking details as expected', async function () {
+        
+        await supportMethods
+            .verifyOutboundBookingDetails(data.noOfOutBoundPassengers, data.noOfOutBoundVehicles);
+        await supportMethods
+            .verifyInboundBookingDetails(data.noOfInBoundPassengers, data.noOfInBoundVehicles);
 
-});
+    });
 
-When('I navigate to outbound sailing page', async function () {
-    
-    await supportMethods
-        .clickOnOutBoundSailing();
-});
+    When('I navigate to outbound sailing page', async function () {
+        
+        await supportMethods
+            .clickOnOutBoundSailing();
+    });
 
-Then('I should see vehicle details as expected', async function () {
-    
-    await supportMethods
-        .verifyOutBoundSailingPassengerDetails('TestOne TestRD', 'TestTwo TestRD');
-});
+    Then('I should see vehicle details as expected', async function () {
+        
+        await supportMethods
+            .verifyOutBoundSailingPassengerDetails(data.leadPassengerDetails, data.otherPassengerDetails);
+    });
 
-Then('I should see lead and other passengers details as expected', async function () {
-    await supportMethods
-        .verifyOutBoundSailingVehicleDetails('Abarth 500 (Car)');
-
+    Then('I should see lead and other passengers details as expected', async function () {
+        
+        await supportMethods
+            .verifyOutBoundSailingVehicleDetails(data.outSailingVehicleDetails);
+    });
 });
